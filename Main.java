c289587554ac504b70fa100393d4063b91c2399a
Andres Jimenez <andres_jimenez@tamu.edu>
2019-10-13 22:29:16 -0500

    added question 2 code

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    private Question2 Q2;
+    public static void main(String[] args) { //this file is not needed in gui just for testing
+        Question2 Q2 = new Question2("2000", "2005", null);
+        System.out.print(Q2.get_answer());
+    }
+}
---
+++ dbSetup.java
@@ -0,0 +1,5 @@
+
+public final class dbSetup  {
+    public static final String user = "andres_jiminez_906";
+    public static final String pswd = "studentpwd";
+}//end class
