f27efc909d265df8c62477610d191c5445766f89
thryke <46359662+thryke@users.noreply.github.com>
2019-10-15 10:47:34 -0500

    Adding Q3 Call

--- BottomToolbar.java
+++ BottomToolbar.java
@@ -1,3 +1,5 @@
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -50,14 +52,22 @@ public class BottomToolbar extends JPanel implements ActionListener {
         String output = "";
         if(clicked == Search) {
             if(sidePanel.Q1B.isSelected()){
-                Q1 question1 = new Q1();
-                String sourceActor = sidePanel.Q1B.sourceActor.getText();
-                question1.FindInitialActor(sourceActor, true); // need to modify this method
-                String destinationActor = sidePanel.Q1b.destinationActor.getText();
-                question1.FindInitialActor(destinationActor, true);
-                String excludedActor = sidePanel.Q1b.excludedActor.getText();
+                Question1.Q1 question1 = new Question1.Q1();
+                String sourceActor = sidePanel.Q1P.sourceActor.getText();
+                //question1.FindInitialActor(sourceActor, true); // need to modify this method
+                String destinationActor = sidePanel.Q1P.destinationActor.getText();
+                //question1.FindInitialActor(destinationActor, true);
+                String excludedActor = sidePanel.Q1P.excludedActor.getText();
                 
-                question1.CreateGraph(excludedActor);
+                question1.CreateGraph(excludedActor, true);
+                /*
+                if(question1.checkForMultipleActors()){
+                    JDialog options = new JDialog();
+                    for(int i=0; i<question1.returnActors().size(); i++){
+                        options.
+                    }
+                }
+                 */
                 output = question1.getShortestPath(); // need to modify return type and have it return string
             }
             else if(sidePanel.Q2B.isSelected()){
@@ -66,7 +76,8 @@ public class BottomToolbar extends JPanel implements ActionListener {
 
             }
             else if(sidePanel.Q3B.isSelected()){
-                //call question 3 here
+                Trivia Q3 = new Trivia(sidePanel.Q3P.name.getText());
+                output = Q3.find_longest();
             }
 
             if(!file.isSelected()){
@@ -85,92 +96,6 @@ public class BottomToolbar extends JPanel implements ActionListener {
                     System.out.print("Error opening new file.");
                 }
             }
-
-            /*
-            // get Query
-            Parser p = new Parser(selectPanel.getD(), selectPanel.getF(), selectPanel.getSearch());
-            p.Select();
-
-            p.Join();
-            p.Where();
-            StringBuffer q = p.getQuery();
-            String sqlStatement = q.toString();
-
-            dbSetup my = new dbSetup();
-            //Building the connection
-            Connection conn = null;
-            try {
-                Class.forName("org.postgresql.Driver");
-                conn = DriverManager.getConnection("jdbc:postgresql://db-315.cse.tamu.edu/trkeller17_906",
-                        my.user, my.pswd);
-            } catch (Exception o) {
-                o.printStackTrace();
-                System.err.println(o.getClass().getName()+": "+o.getMessage());
-                System.exit(0);
-            }//end try catch
-            String output = "";
-            try{
-                //create a statement object
-                Statement stmt = conn.createStatement();
-                System.out.print(sqlStatement + "\n");
-                //send statement to DBMS
-                ResultSet results = stmt.executeQuery(sqlStatement);
-                //OUTPUT
-                //System.out.println("______________________________________");
-                ResultSetMetaData metaData = results.getMetaData();
-                int colCount = metaData.getColumnCount();
-                String[] names = new String[colCount];
-                for (int i =1; i<= colCount; i++){
-                    String colName = metaData.getColumnName(i);
-                    output += colName;
-                    names[i-1] = colName;
-                    if(i != colCount){
-                        output += ", ";
-                    }
-                    else{
-                        output += '\n';
-                    }
-                }
-                while (results.next()) {
-                    //System.out.println(results.getString("primaryname"));
-                    for(int i = 0; i < colCount; i++) {
-                        output += results.getString(names[i]);
-                        if(i != colCount-1){
-                            output += ", ";
-                        }
-                        else{
-                            output += '\n';
-                        }
-                    }
-                    output += "\n";
-                }
-            } catch (Exception o){
-                System.out.print("Error accessing Database.");
-            }
-            if(!file.isSelected()){
-                resultPanel.setResult(output);
-            }
-            else{
-                FileOutputStream out = null;
-                try {
-                    out = new FileOutputStream(filename.getText());
-                    byte b[] = output.getBytes();
-                    out.write(b);
-                    out.close();
-                    resultPanel.setResult("Results in file: " + filename.getText() + "\n");
-                }
-                catch (Exception o){
-                    System.out.print("Error opening new file.");
-                }
-            }
-
-            //closing the connection
-            try {
-                conn.close();
-            } catch(Exception o) {
-                System.out.print("connection closed\n");
-            }//end try catch
-            */
         }
         if(clicked == Exit){
             System.exit(0);
