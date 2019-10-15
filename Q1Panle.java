fe08c73158136c319b58fce762e06204cb3b100a
TrasherW571 <50845526+TrasherW571@users.noreply.github.com>
2019-10-15 09:10:35 -0500

    Update Q1Panle.java

--- Q1Panle.java
+++ Q1Panle.java
@@ -17,9 +17,10 @@ public class Q1Panle extends JPanel {
     public Q1Panle() {
         sourceActor = new JTextArea(1,15);
         destinationActor = new JTextArea(1,15);
-        //excludedActor = new JTextArea(1,15);
+        excludedActor = new JTextArea(1,15);
         heading_sourceActor = new JTextArea("Source Actor:");
         heading_destinationActor = new JTextArea("Destination Actor:");
+        heading_excludedActor = new JTextArea("Excluded Actor:");
         
         setLayout(new GridLayout(6,1));
         setBorder(new CompoundBorder(new EmptyBorder(3, 3, 3, 3), BorderFactory.createLineBorder(Color.GRAY)));
@@ -30,11 +31,17 @@ public class Q1Panle extends JPanel {
         heading_destinationActor.setEditable(false);
         heading_destinationActor.setBackground(new Color(0,0,0,0));
         
+        heading_excludedActor.setEditable(false);
+        heading_excludedActor.setBackground(new Color(0,0,0,0));
+        
         sourceActor.setBorder(new CompoundBorder(new EmptyBorder(1, 1, 1, 1), BorderFactory.createLineBorder(Color.GRAY)));
         destinationActor.setBorder(new CompoundBorder(new EmptyBorder(1, 1, 1, 1), BorderFactory.createLineBorder(Color.GRAY)));
+        excludedActor.setBorder(new CompoundBorder(new EmptyBorder(1, 1, 1, 1), BorderFactory.createLineBorder(Color.GRAY)));
         
         add(heading_sourceActor);
         add(sourceActor);
         add(heading_destinationActor);
         add(destinationActor);
+        add(heading_excludedActor);
+        add(excludedActor);
 }
