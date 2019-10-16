3d04c0f8ba7c898018e8508598daf9f6e56e5d58
thryke <46359662+thryke@users.noreply.github.com>
2019-10-16 13:08:24 -0500

    fixing now that we can test

--- Trivia.java
+++ Trivia.java
@@ -10,49 +10,58 @@ import java.util.Arrays;
 
 public class Trivia {
     private Connect data;
-    private String solution;
+    private Connect data2;
     public String inputName;
 
     public Trivia(String inputName) {
         this.inputName = inputName;
-        data = new Connect();
+        //data = new Connect();
+        //data2 = new Connect();
         this.find_longest();
     }
 
     public String find_longest() {
         inputName = this.inputName;
-        String sqlStatement = "SELECT primarytitle, runtimeminutes FROM titlebasics JOIN principals ON principals.tconst = titlebasics.tconst JOIN names ON principals.nconst = names.nconst WHERE primaryname='";
+        data = new Connect();
+        data2 = new Connect();
+        String sqlStatement = "SELECT runtimeminutes FROM titlebasics JOIN principals ON principals.tconst = titlebasics.tconst JOIN names ON principals.nconst = names.nconst WHERE primaryname='";
+        String sqlStatement2 = "SELECT primarytitle FROM titlebasics JOIN principals ON principals.tconst = titlebasics.tconst JOIN names ON principals.nconst = names.nconst WHERE primaryname='";
         sqlStatement += inputName;
         sqlStatement += "' AND titletype='movie';";
-        System.out.println(sqlStatement);
-        dbSetup my = new dbSetup();
+
+        sqlStatement2 += inputName;
+        sqlStatement2 += "' AND titletype='movie';";
+
         data.ConnectToDatabase();
-        System.out.println("Connected");
         data.QueryDatabase(sqlStatement);
         ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
         result = data.getQueryResult();
         data.CloseConnection();
 
+        data2.ConnectToDatabase();
+        data2.QueryDatabase(sqlStatement2);
+        ArrayList<ArrayList<String>> result2 = new ArrayList<ArrayList<String>>();
+        result2 = data2.getQueryResult();
+        data2.CloseConnection();
+
         System.out.println(result.get(0));
+        System.out.println(result2.get(0));
+
         int longestFilm = 0;
         String filmName = "";
 
         for (int i = 0; i < result.get(0).size(); i++) {
             if (checkIfInteger(result.get(0).get(i))) {
-                if (Integer.parseInt(result.get(0).get(i)) > longestFilm && !(result.get(0).get(i - 1).equals("0"))) {
+                if (Integer.parseInt(result.get(0).get(i)) > longestFilm) {
                     longestFilm = Integer.parseInt(result.get(0).get(i));
-                    filmName = result.get(0).get(i - 1);
-                    System.out.println(longestFilm);
+                    filmName = result2.get(0).get(i);
+
                 }
             }
         }
 
-        if(!(solution.equals(""))) {
-            solution = "The longest film " + inputName + " has been in is " + filmName + ", with a runtime of " + longestFilm + " minutes.";
-            return solution;
-        }
-        else
-            return null;
+        String solution = "The longest film " + inputName + " has been in is " + filmName + ", with a runtime of " + longestFilm + " minutes.";
+        return solution;
     }
 
     private Boolean checkIfInteger(String s){
